82f5f484e56152c8f6771abb2c2f0ee2ee8fd5a6
KyoufuuN1K1 <54867136+KyoufuuN1K1@users.noreply.github.com>
2019-11-19 11:12:38 -1000

    Updated Landing

--- app/imports/ui/pages/UserLanding.jsx
+++ app/imports/ui/pages/UserLanding.jsx
@@ -3,11 +3,16 @@ import PropTypes from 'prop-types';
 import { withTracker } from 'meteor/react-meteor-data';
 import { withRouter } from 'react-router-dom';
 import { Meteor } from 'meteor/meteor';
-import { Grid, Icon, Header } from 'semantic-ui-react';
+import { Grid, Icon, Header, Tab } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class UserLanding extends React.Component {
   render() {
+    const panes = [
+      { menuItem: 'ICS101', pane: 'ICS101 Content' },
+      { menuItem: 'ICS102', pane: 'ICS102 Content' },
+      { menuItem: 'ICS103', pane: 'ICS103 Content' },
+    ];
     return (
         <div className="digits-landing-background">
           <Header as="h1" centered inverted>Welcome, {this.props.currentUser}</Header>
@@ -17,13 +22,13 @@ class UserLanding extends React.Component {
             <Icon size="huge" name="users" inverted/>
             <Header as="h1" inverted>Classmates</Header>
             {/* eslint-disable-next-line max-len */}
-            <Header as="h3" inverted>This address book enables any number of users to register and save their business contacts. You can only se the contacts you have created.</Header>
+            <Tab panes={panes} renderActiveOnly={false} />
           </Grid.Column>
 
           <Grid.Column textAlign='center'>
             <Icon size="huge" name="file alternate" inverted/>
             <Header as="h1" inverted>Class Details</Header>
-            <Header as="h3" inverted>For each contact, you can save their name, address, and phone number.</Header>
+            <Tab panes={panes} renderActiveOnly={false} />
           </Grid.Column>
 
           <Grid.Column textAlign='center'>
