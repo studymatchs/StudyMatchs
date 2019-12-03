d3c2597f033b05f452bbc1b5d758beffbb98c126
Jamse Galway-Sevetson <jtmoka@hawaii.edu>
2019-12-03 11:09:30 -1000

    good

--- app/imports/ui/pages/ListMessages.jsx
+++ app/imports/ui/pages/ListMessages.jsx
@@ -18,17 +18,17 @@ class ListMessages extends React.Component {
 
   /** Render the page once subscriptions have been received. */
   renderPage() {
-    console.log(this.props.messages.Name);
+    console.log("Hello");
     return (
           <div className="studymachs-message-background-image">
             <Container>
               <Header as="h2" textAlign="center">Message Boards</Header>
               <Card.Group>
                 {/* eslint-disable-next-line max-len */}
-                {this.props.messages.map((message, index) => <Modal trigger={<Button>{message.Name} Board</Button>}><Message key={index} message={message} notes={this.props.notes.filter(note => (note.contactId === message._id))} /> /></Modal>)}
+                {this.props.messages.map((message, index) => <Modal trigger={<Button>{message.Name} Board</Button>}><Message key={index} message={message} notes={this.props.notes.filter(note => (note.contactId === message._id))} /> So..? </Modal>)}
               </Card.Group>
 
-              <div className="ui center aligned container foot">
+              <div className="ui center aligned container studymachs-message-foot">
                 <Button as={NavLink} activeClassName="active" exact to="/AddMessage" key='AddMessage'>Make Message Board</Button>
               </div>
 
