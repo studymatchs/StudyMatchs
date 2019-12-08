ae06aaf012c5f5f87cc9573c75cae9d7d90ef82c
Jamse Galway-Sevetson <jtmoka@hawaii.edu>
2019-12-08 07:40:01 -1000

    Not done

--- app/imports/ui/pages/ListMessages.jsx
+++ app/imports/ui/pages/ListMessages.jsx
@@ -11,6 +11,10 @@ import { NavLink } from 'react-router-dom';
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class ListMessages extends React.Component {
 
+
+  deleteThisMessage() {
+    Messages.remove(this.props.message._id);
+  }
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
     return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
@@ -25,7 +29,13 @@ class ListMessages extends React.Component {
               <Header as="h2" textAlign="center">Message Boards</Header>
               <Card.Group>
                 {/* eslint-disable-next-line max-len */}
-                {this.props.messages.map((message, index) => <Modal trigger={<Button>{message.Name} Board</Button>}><Message key={index} message={message} notes={this.props.notes.filter(note => (note.contactId === message._id))} /> <Button as={NavLink} activeClassName="active" exact to="/AddMessage" key='AddMessage'>Make Message Board</Button> </Modal>)}
+                {this.props.messages.map((message, index) => <Modal trigger={<Button>{message.Name} Board</Button>}>
+                  <Message key={index} message={message} notes={this.props.notes.filter(note => (note.contactId === message._id))} />
+                <Button as={NavLink} activeClassName="active" exact to="/AddMessage" key='AddMessage'>Make Message Board</Button>
+                  <Button icon onClick={this.deleteThisMessage.bind(this)}>
+                  <Icon name='trash' />
+                </Button>
+                </Modal>)}
               </Card.Group>
 
               <div className="ui center aligned container studymachs-message-foot">
