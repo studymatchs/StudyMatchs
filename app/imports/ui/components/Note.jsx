f72008a7a979ffb46fb679e88defa14ad46a5a9e
Jamse Galway-Sevetson <jtmoka@hawaii.edu>
2019-12-13 20:11:16 -1000

    beter

--- app/imports/ui/components/Note.jsx
+++ app/imports/ui/components/Note.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Feed, List } from 'semantic-ui-react';
+import { Button, Feed, Icon } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import swal from 'sweetalert';
@@ -7,9 +7,29 @@ import { Notes } from '../../api/note/Notes';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class Note extends React.Component {
+
+
+  delete =() => {
+    swal({
+      title: 'Are you sure?',
+      text: 'Once deleted, you will not be able to recover this message!',
+      icon: 'warning',
+      buttons: true,
+      dangerMode: true,
+    })
+        .then((willDelete) => {
+          if (willDelete) {
+            Notes.remove(this.props.note._id);
+            swal('The message has been deleted!', {
+              icon: 'success',
+            });
+          } else {
+            swal('Your imaginary file is safe!');
+          }
+        });
+  };
+
   render() {
-
-
 
     return (
         <Feed.Event >
@@ -19,13 +39,24 @@ class Note extends React.Component {
               <Feed.Date content={this.props.note.createdAt.toLocaleDateString('en-US')} />
             </Feed.Summary>
           </Feed.Content>
+
           <Feed.Content>
             <Feed.Summary>
               {this.props.note.note}
 
             </Feed.Summary>
           </Feed.Content>
+
+          <Feed.Content>
+            <Feed.Summary>
+              <Button animated onClick={this.delete}>
+                <Button.Content visible><Icon name='trash' /></Button.Content>
+                <Button.Content hidden>delete</Button.Content>
+              </Button>
+            </Feed.Summary>
+          </Feed.Content>
         </Feed.Event>
+
     );
   }
 }
