6c970a95e41c5b29cccd5b8d89b3b677eb23005e
yatfu <keanuyl@hawaii.edu>
2019-12-16 09:27:44 -1000

    Added edit and delete, improved ui
    Fully functioning edit and delete functions w/ admin and owner, changed list to cards

--- app/imports/api/studysession/StudySessions.js
+++ app/imports/api/studysession/StudySessions.js
@@ -13,8 +13,8 @@ const StudySessionSchema = new SimpleSchema({
   description: String,
   subject: {
     type: String,
-    allowedValues: ['ICS', 'Math', 'Biology', 'Chemistry', 'Physics', 'Art/Design', 'Performing Arts', 'Other'],
-    defaultValue: 'Other',
+    allowedValues: ['Select one', 'ICS', 'Math', 'Biology', 'Chemistry', 'Physics', 'Art/Design', 'Performing Arts', 'Environmental Sci', 'Business/Econ', 'Law', 'Nursing', 'Medical', 'Other'],
+    defaultValue: 'Select one',
   },
   SOS: {
     type: Boolean,
@@ -23,10 +23,6 @@ const StudySessionSchema = new SimpleSchema({
   finished: {
     type: Boolean,
     defaultValue: false,
-  },
-  team: {
-    type: String,
-    defaultValue: [''],
   },
   owner: String,
 }, { tracker: Tracker });
--- app/imports/ui/components/StudySession.jsx
+++ app/imports/ui/components/StudySession.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { List } from 'semantic-ui-react';
+import {Card, List} from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
+import {Link, withRouter} from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class StudySession extends React.Component {
@@ -11,14 +11,11 @@ class StudySession extends React.Component {
             <List.Item>
                 <List.Icon name='marker' />
                 <List.Content>
-                    <List.Header as='a'>{this.props.studysession.name} by
-                        {this.props.studysession.owner}</List.Header>
+                    <List.Header as='a'>{this.props.studysession.name} by {this.props.studysession.time}
+                        {this.props.studysession.owner} at </List.Header>
                     <List.Description>
                         {this.props.studysession.description}
                     </List.Description>
-                <List.Content extra>
-                    {this.props.studysession.date}
-                </List.Content>
                 </List.Content>
             </List.Item>
         </List>
