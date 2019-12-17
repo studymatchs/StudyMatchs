2af2dadec727b933d487a4c4647e90deb7c5ccbe
KyoufuuN1K1 <54867136+KyoufuuN1K1@users.noreply.github.com>
2019-12-17 04:51:29 -1000

    Update ClassList.jsx

--- app/imports/ui/components/ClassList.jsx
+++ app/imports/ui/components/ClassList.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
 import { Button, Feed, Container, Segment, Header, List, Label } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import AddClass from './AddClass';
+import { Classmates } from '../../api/classes/Classmates';
+import { UserClasses } from '../../api/profile/UserClasses';
+
+function classWithdraw(name, student) {
+  const owner = Meteor.user().username;
+  const profileID = UserClasses.findOne({ userID: owner })._id;
+  if (name !== 'None') {
+    const studentID = Classmates.findOne({className: name, classmate: student})._id;
+    Classmates.remove(studentID);
+  }
+  UserClasses.update(profileID, { $pull: { classes: name } });
+}
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class ClassList extends React.Component {
@@ -16,7 +29,7 @@ class ClassList extends React.Component {
               <List divided relaxed>
                 {/* eslint-disable-next-line max-len */}
                 {this.props.ClassList.map((classStuff, index) => <Label key={index}>
-                  <Button icon='cancel'/> {classStuff}
+                  <Button icon='cancel' onClick={() => classWithdraw( classStuff, this.props.user.userID)}/> {classStuff}
                 </Label>)}
               </List>
             </Segment>
