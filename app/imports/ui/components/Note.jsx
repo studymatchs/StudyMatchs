89ed3c19b685407eba05a8961984013dd42d417c
Jamse Galway-Sevetson <jtmoka@hawaii.edu>
2019-12-16 18:03:58 -1000

    no note

--- app/imports/ui/components/Note.jsx
+++ app/imports/ui/components/Note.jsx
@@ -34,7 +34,7 @@ class Note extends React.Component {
 
 /** Require a document to be passed to this component. */
 Note.propTypes = {
-  note: PropTypes.array.isRequired,
+  
 };
 
 /** Wrap this component in withRouter since we use the <Link> React Router element. */
